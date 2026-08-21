b6ed9405385d2e2b8102cac52354bbf83babbeae
VijayKumar <vijayarya326@gmail.com>
2026-08-21 11:14:02 +0530

    validated sandbox and live token to start further processing

--- src/helper_func/manage_login.py
+++ src/helper_func/manage_login.py
@@ -8,9 +8,9 @@ from helper_func.upstox_requests import login, sandbox_token_active
 from pathlib import Path
 
 def check_user_auth(env_path: Path) -> None:
-    # TODO: Implement Auth and relogin of token expired
     active_token =  sandbox_token_active(forceProd= True)
     if active_token == False :
+        #If invalid token, attempting relogin
         login(client_id= UPSTOX_CLIENT_ID,
                 client_secret=UPSTOX_CLIENT_SECRET,
                 redirect_url= UPSTOX_REDIRECT_URI,
