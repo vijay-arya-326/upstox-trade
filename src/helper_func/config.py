6a3e6c2ac9d82322396215caf7666198d37f1c83
VijayKumar <vijayarya326@gmail.com>
2026-08-25 10:00:08 +0530

    creating db and connecting to database - sqllite

--- src/helper_func/config.py
+++ src/helper_func/config.py
@@ -62,5 +62,7 @@ INSTRUMENT_KEY =  "NSE_FO|61703"
 DB_PATH_FULL = Path(__file__).parent.parent.joinpath(os.getenv("DB_PATH"))
 
 if not DB_PATH_FULL.exists():
-    DB_PATH_FULL.mkdir(parents=True, exist_ok=True)
+    DB_PATH_FULL.parent.mkdir(parents=True, exist_ok=True)
+    DB_PATH_FULL.touch(exist_ok=True)
     print(f"Database path created at {DB_PATH_FULL}")
+
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from DTO.order_model import OrderModel, ModifyOrderModel
 from bootstrap.pre_load_check import check_pre_env
 import sys
 
+from db.helper.db_connector import db_session
 from helper_func.brokerage import calculate_brokerage
 from helper_func.config import APPNAME, LOADED_ENV, ENV_PATH, INSTRUMENT_KEY
 from helper_func.download_assets import download_nse_file
@@ -9,6 +10,7 @@ from helper_func.fancy_print import fancy_print
 from helper_func.manage_login import check_user_auth, validate_sandbox_token
 from helper_func.constants import SANDBOX_ENV_NAME
 from helper_func.order_helper import place_order, get_order_detail, cancel_order, modify_order
+from db.helper.db_connector import db_session
 
 if __name__ == "__main__":
     fancy_print(APPNAME, border_color="green")
@@ -18,22 +20,27 @@ if __name__ == "__main__":
         if not validate_sandbox_token():
             sys.exit(1)
 
-    order_obj_buy = {
-        "instrument_token": INSTRUMENT_KEY,
-        "product": "D",
-        "quantity": 65,
-        "transaction_type": "BUY",
-        "price": 11000
-    }
+    with db_session() as session:
+        session.execute("Select 1;")
+        fancy_print(msg="DB Connected", title="DB Connected", border_color="green")
 
-    calculate_brokerage(order_obj= order_obj_buy)
 
-    order_obj_sell = {
-        "instrument_token": INSTRUMENT_KEY,
-        "product": "D",
-        "quantity": 65,
-        "transaction_type": "SELL",
-        "price": 12000
-    }
-
-    calculate_brokerage(order_obj=order_obj_sell)
+    # order_obj_buy = {
+    #     "instrument_token": INSTRUMENT_KEY,
+    #     "product": "D",
+    #     "quantity": 65,
+    #     "transaction_type": "BUY",
+    #     "price": 11000
+    # }
+    #
+    # calculate_brokerage(order_obj= order_obj_buy)
+    #
+    # order_obj_sell = {
+    #     "instrument_token": INSTRUMENT_KEY,
+    #     "product": "D",
+    #     "quantity": 65,
+    #     "transaction_type": "SELL",
+    #     "price": 12000
+    # }
+    #
+    # calculate_brokerage(order_obj=order_obj_sell)
